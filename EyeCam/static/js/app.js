be92b0c0fad5b2cfbd039dde37d5c73612fff8ba
chaiyuntian <tianyunchai@126.com>
2015-03-24 20:27:17 +0800

    Added camera support.

--- EyeCam/static/js/app.js
+++ EyeCam/static/js/app.js
@@ -4,7 +4,7 @@
 
 var APP = {
     version:0.1,
-    dependency:pupil
+    dependency:[pupil,camera]
 
 };
 /*
@@ -25,7 +25,7 @@ circle.prototype.changeColor=function(c)
 
 
 var config = function(){
-    this.distance = 10;
+    this.distance = 100;
 
 };
 
@@ -33,7 +33,33 @@ config.prototype.Save=function(k,v)
 {
     this[k] = v;
     // Save to local storage
+    window.localStorage[k] = v;
 };
+
+
+
+//进入全屏
+function fullScreen() {
+    var de = document.documentElement;
+    if (de.requestFullscreen) {
+        de.requestFullscreen();
+    } else if (de.mozRequestFullScreen) {
+        de.mozRequestFullScreen();
+    } else if (de.webkitRequestFullScreen) {
+        de.webkitRequestFullScreen();
+    }
+}
+//退出全屏
+function exitFullscreen() {
+    var de = document;
+    if (de.exitFullscreen) {
+        de.exitFullscreen();
+    } else if (de.mozCancelFullScreen) {
+        de.mozCancelFullScreen();
+    } else if (de.webkitCancelFullScreen) {
+        de.webkitCancelFullScreen();
+    }
+}
 
 
 // canvas tha manage the 9 points on the screen
@@ -44,11 +70,14 @@ var Cvs = function(cvsId)
     this.canvas.width=window.innerWidth;
     this.canvas.height=window.innerHeight;
     this.ctx = this.canvas.getContext('2d');
+
+
     //return this;
 };
 
 Cvs.prototype.clear=function()
 {
+    this.ctx.fillStyle="#000000";
     this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
 };
 
@@ -86,16 +115,13 @@ var Circles = function(w,h,sp,r,c)
 
     // center row
     circles.push(new circle(left,cy,r,c));
-    circles.push(new circle(right,cy,r,c));
     circles.push(new circle(cx,cy,r,c));
-
+    circles.push(new circle(right,cy,r,c));
 
     // bottom row
-
     circles.push(new circle(left,bottom,r,c));
     circles.push(new circle(cx,bottom,r,c));
     circles.push(new circle(right,bottom,r,c));
-
     this.children = circles;
 
 
@@ -115,14 +141,131 @@ Circles.prototype.drawOne= function(ctx,index)
     drawCircle(ctx,this.children[index]);
 };
 
+var process = function(imgdata,w,h){
+
+        var  grayData= pupil.gs(imgdata.data);
+        //var  grayInvs = pupil.invs(grayData);
 
 
-var canvas = new Cvs();
-canvas.clear();
-var cfg = new config();
-var circles = new Circles(canvas.canvas.width,canvas.canvas.height,cfg.distance,5,"#FF0000");
-//circles.drawAll(canvas.ctx);
-circles.drawOne(canvas.ctx,1);
+        //var  grayBin = pupil.bin(grayInvs,200,0);
+        //var  grayCny = pupil.cny(grayData,w,h);
+        //var  grayCut2 = pupil.bin(grayCny,150);
+        var  graySb = pupil.sbl(grayData,w,h,pupil.kernel.k0);
+
+        var  thres = pupil.fT(pupil.hist(graySb),w*h,0.1);
+
+        var  grayCut = pupil.bin(graySb,thres);
+        //var  grayCut = pupil.dcut(grayCny,135,0);
+        var  data = graySb;
+
+        return data;
+
+
+};
+
+
+
+var main=function()
+{
+    var cfg = new config();
+    var canvas = new Cvs();
+    canvas.clear();
+    var circles = new Circles(canvas.canvas.width,canvas.canvas.height,cfg.distance,5,"#FF0000");
+    circles.drawAll(canvas.ctx);
+    // Handle Resize Event
+    window.addEventListener( 'resize', function(){
+        canvas.canvas.width=window.innerWidth;
+        canvas.canvas.height=window.innerHeight;
+        circles = new Circles(canvas.canvas.width,canvas.canvas.height,cfg.distance,5,"#FF0000");
+        canvas.clear();
+        circles.drawAll(canvas.ctx);
+
+    }, false );
+
+
+    // For Test Usage
+    function keydown(e)
+    {
+        /*
+        keycode 49 = 1 exclam onesuperior
+        keycode 50 = 2 quotedbl twosuperior
+        keycode 51 = 3 section threesuperior
+        keycode 52 = 4 dollar
+        keycode 53 = 5 percent
+        keycode 54 = 6 ampersand
+        keycode 55 = 7 slash braceleft
+        keycode 56 = 8 parenleft bracketleft
+        keycode 57 = 9 parenright bracketright
+        */
+
+        var keyID = e.keyCode ? e.keyCode :e.which;
+        if(keyID>=49&&keyID<=57) {
+            canvas.clear();
+            circles.drawOne(canvas.ctx, keyID - 49);
+            /* TODO:Test the proper delay time and Set getImage in a delay callback
+             *
+             */
+
+            getimage = true;
+        }
+
+        if(keyID===32)
+        {
+            fullScreen();
+        }
+
+    }
+
+
+
+
+//circles.drawOne(canvas.ctx,1);
 
 // TO DO
 // Add Camera Read and take screen shot
+
+    var getimage = false;
+    var imageList = [];
+    var w = 640,h = 480;
+    camera.init({
+        width: w, // default: 640
+        height: h, // default: 480
+        fps: 30, // default: 30
+        mirror: true,  // default: false
+        targetCanvas: null, // default: null
+
+        onFrame: function(canvas) {
+            // do something with image data found in the canvas argument
+            if(getimage){
+                getimage = false;
+                var data = canvas.getContext('2d').getImageData(0,0,w,h);
+                var img = process(data,w,h);
+                imageList.push(img);
+
+                console.log(imageList.length);//For test.
+            }
+        },
+
+        onSuccess: function() {
+            // stream succesfully started, yay!
+        },
+
+        onError: function(error) {
+            // something went wrong on initialization
+        },
+
+        onNotSupported: function() {
+            // instruct the user to get a better browser
+        }
+    });
+
+
+    window.addEventListener('keydown', keydown,true);
+
+    camera.start();
+};
+
+main();
+
+
+
